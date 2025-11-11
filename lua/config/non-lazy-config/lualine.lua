a00caf60c17d7234cbaefb442adadc0b5baa5006
Amos Thacker-Gwaltney <amostg3613@gmail.com>
2025-11-11 13:43:24 -0500

    latex math notes support, lualine mods, 2space tab

--- lua/config/non-lazy-config/lualine.lua
+++ lua/config/non-lazy-config/lualine.lua
@@ -1,34 +1,81 @@
+--local colors = {
+--  blue   = '#80a0ff',
+--  cyan   = '#79dac8',
+--  black  = '#080808',
+--  white  = '#c6c6c6',
+--  red    = '#ff5189',
+--  violet = '#d183e8',
+--  grey   = '#303030',
+--}
+--
+--local bubbles_theme = {
+--  normal = {
+--    a = { fg = colors.orange], bg = 'none' },
+--    b = { fg = colors.orange], bg = 'none' },
+--    c = { fg = colors.orange] },
+--  },
+--
+--  insert = { a = { fg = colors.orange], bg = 'none'  } },
+--  visual = { a = { fg = colors.orange], bg = 'none' } },
+--  replace = { a = { fg = colors.orange], bg = 'none' } },
+--
+--  inactive = {
+--    a = { fg = colors.orange], bg = 'none' },
+--    b = { fg = colors.orange], bg = 'none' },
+--    c = { fg = colors.orange] },
+--  },
+--}
+
 local colors = {
-  blue   = '#80a0ff',
-  cyan   = '#79dac8',
-  black  = '#080808',
-  white  = '#c6c6c6',
-  red    = '#ff5189',
-  violet = '#d183e8',
-  grey   = '#303030',
+  black        = '#282828',
+  white        = '#ebdbb2',
+  red          = '#fb4934',
+  orange       = '#FFA500',
+  green        = '#b8bb26',
+  blue         = '#83a598',
+  yellow       = '#fe8019',
+  gray         = '#a89984',
+  darkgray     = '#3c3836',
+  lightgray    = '#504945',
+  inactivegray = '#7c6f64',
 }
 
-local bubbles_theme = {
+local gruvbox_theme = {
   normal = {
-    a = { fg = colors.white, bg = 'none' },
-    b = { fg = colors.white, bg = 'none' },
-    c = { fg = colors.white },
+    a = { bg = 'none', fg = colors.orange, gui = 'bold'},
+    b = { bg = 'none', fg = colors.orange},
+    c = { bg = 'none', fg = colors.orange}
   },
-
-  insert = { a = { fg = colors.white, bg = 'none' } },
-  visual = { a = { fg = colors.white, bg = 'none' } },
-  replace = { a = { fg = colors.white, bg = 'none' } },
-
+  insert = {
+    a = { bg = 'none', fg = colors.orange, gui = 'bold'},
+    b = { bg = 'none', fg = colors.orange},
+    c = { bg = 'none', fg = colors.orange}
+  },
+  visual = {
+    a = { bg = 'none', fg = colors.orange, gui = 'bold'},
+    b = { bg = 'none', fg = colors.orange},
+    c = { bg = 'none', fg = colors.orange}
+  },
+  replace = {
+    a = { bg = 'none', fg = colors.orange, gui = 'bold'},
+    b = { bg = 'none', fg = colors.orange},
+    c = { bg = 'none', fg = colors.orange}
+  },
+  command = {
+    a = { bg = 'none', fg = colors.orange, gui = 'bold'},
+    b = { bg = 'none', fg = colors.orange},
+    c = { bg = 'none', fg = colors.orange}
+  },
   inactive = {
-    a = { fg = colors.white, bg = 'none' },
-    b = { fg = colors.white, bg = 'none' },
-    c = { fg = colors.white },
-  },
+    a = { bg = 'none', fg = colors.orange, gui = 'bold'},
+    b = { bg = 'none', fg = colors.orange},
+    c = { bg = 'none', fg = colors.orange}
+  }
 }
 
 require('lualine').setup {
 	options = {
-		theme = bubbles_theme,
+		theme = gruvbox_theme,
 		icons_enabled = true,
     component_separators = "",
     section_separators = "",
---
+++ lua/plugins/vimtex.lua
@@ -0,0 +1,11 @@
+return {
+  {
+    "lervag/vimtex",
+    lazy = false,
+    ft = "tex",
+    config = function()
+      vim.g.vimtex_view_method = "zathura"
+      vim.g.vimtex_quickfix_open_on_warning = 0
+    end
+  }
+}
