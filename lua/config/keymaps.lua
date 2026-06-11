470ea45bfeb67e2a80b350cf72318fdf57969f53
Amos <morphouswatery@gmail.com>
2026-06-11 13:52:24 -0400

    added telescope back into config

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -51,3 +51,10 @@ map("n", "<leader>ntw", "<cmd>e ./<CR>")
 
 -- lspbinds
 map("n", "gd", "<cmd>lua vim.lsp.buf.definition()<CR>")
+
+-- telescope
+local builtin = require('telescope.builtin')
+map('n', '<leader>ff', builtin.find_files, { desc = 'Telescope find files' })
+map('n', '<leader>fg', builtin.live_grep, { desc = 'Telescope live grep' })
+map('n', '<leader>fb', builtin.buffers, { desc = 'Telescope buffers' })
+map('n', '<leader>fh', builtin.help_tags, { desc = 'Telescope help tags' })
