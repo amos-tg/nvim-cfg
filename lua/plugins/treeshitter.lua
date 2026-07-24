5a4c7f8606ac047259e085f9fbb7757f976af89e
Amos <morphouswatery@gmail.com>
2026-07-24 14:59:48 -0400

    fixed treesitter build cmd, keymaps

--- lua/plugins/treeshitter.lua
+++ lua/plugins/treeshitter.lua
@@ -2,8 +2,6 @@ return {
 	{
 		"nvim-treesitter/nvim-treesitter",
     branch = "main",
-		build = function()
-			require("nvim-treesitter.install").update({ with_sync = true })()
-		end,
+		build = ":TSUpdate",
 	}
 }
